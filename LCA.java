5716c69e27e0b2c62a3729afce59beb43c505bdf
patrickByrne99 <47981108+patrickByrne99@users.noreply.github.com>
2019-09-30 22:11:51 +0100

    Update LCA.java

--- LCA.java
+++ LCA.java
@@ -21,4 +21,14 @@ public class LCA <Key extends Comparable<Key>, Value>{
 		return size() == 0;
 	}
 	
-	
+	public int size() 
+	{
+		  return(size(root)); 
+		}
+		private int size(Node node) 
+		{ 
+		  if (node == null) return(0); 
+		  else { 
+		    return(size(node.left) + 1 + size(node.right)); 
+		  } 
+		} 
